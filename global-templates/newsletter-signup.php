d3688f586e57564a335e23241799008d4f88571d
tolkadot <tolkadot@gmail.com>
2019-08-27 20:48:31 +1000

    Adding Mailchimp

--- global-templates/newsletter-signup.php
+++ global-templates/newsletter-signup.php
@@ -1,19 +1,20 @@
-<?php ?>
 <div id="mc_embed_signup" class="mc-newsletter">
-<form action="" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
-  <div id="mc_embed_signup_scroll" class="w-250 mx-auto mx-sm-0">
-    <input type="text" value="" name="FNAME" class="name w-100 mb-1" id="mce-FNAME" placeholder="name" >
+<form action="https://happilymadeartisantextiles.us3.list-manage.com/subscribe/post-json?u=f653a3964235dfcb5b392cb65&amp;id=1bc83160f9&c=" method="get" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"  novalidate="">
+  <div id="mc_embed_signup_scroll" class="mx-auto mx-sm-0">
+    <input type="text" value="" name="FNAME" class="name w-100 mb-1" id="mce-FNAME" placeholder="first name" >
   </div>
-  <div id="newsletter-submit-container" class="w-250 mx-auto mx-sm-0">
+  <div id="newsletter-submit-container" class="mx-auto mx-sm-0">
 	  <input type="email" value="" name="EMAIL" class="email w-100 mb-1" id="mce-EMAIL" placeholder="email" width="100%" required="" >
   </div>
-<div class="clear mailing-list-signup-form-cta w-250 mx-auto mx-sm-0">
+<div class="clear mailing-list-signup-form-cta  mx-auto mx-sm-0">
   <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="mailing-list-signup-form-submit btn btn-outline-primary w-100">
 </div>
-
-
-
+<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>
     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_302b7fc3cb039a9e08bba4cb3_79127a28c0" tabindex="-1" value=""></div>
-</form>
+		<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f653a3964235dfcb5b392cb65_1bc83160f9" tabindex="-1" value=""></div>
+		</form>
 </div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
